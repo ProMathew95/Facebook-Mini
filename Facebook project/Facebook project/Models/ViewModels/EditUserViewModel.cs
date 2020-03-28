03702bc0871482e5bc80418e547ffd491ff06233
mathew samir <mathew_samir777@yahoo.com>
2020-03-28 23:51:15 +0200

    Add Admin functionality for app

--- Facebook project/Facebook project/Models/ViewModels/EditUserViewModel.cs
+++ Facebook project/Facebook project/Models/ViewModels/EditUserViewModel.cs
@@ -6,19 +6,42 @@ using System.Threading.Tasks;
 
 namespace Facebook_project.Models.ViewModels
 {
+
+
+
+
     public class EditUserViewModel
     {
-        public EditUserViewModel()
-        {
-            Roles = new List<string>();        
-        }
-        public string id { get; set; }
-        [Required]
-        public string UserName { get; set; }
-        [Required][EmailAddress]
-        public string Email { get; set; }
-       
 
-        public IList<string> Roles { get;  set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string id { get; set; }
+
+
+
+        //public EditUserViewModel()
+        //{
+        //    Roles = new List<string>();
+        //}
+
+
+
+        //[Required]
+        //public string UserName { get; set; }
+        //[Required]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
+
+        //public IList<string> Roles { get; set; }
     }
 }
